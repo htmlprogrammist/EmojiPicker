24f256bf7e8633c51010eca6ef9133b1fc4ec459
Egor Badmaev <60363270+htmlprogrammist@users.noreply.github.com>
2023-01-14 12:59:24 +0300

    refactor: refuse to `Configuration` usage

--- Example App/iOS Example/ViewController.swift
+++ Example App/iOS Example/ViewController.swift
@@ -26,8 +26,8 @@ class ViewController: UIViewController {
     }
     
     @objc private func openEmojiPickerModule(sender: UIButton) {
-        let configuration = Configuration(delegate: self, sender: sender)
-        let viewController = EmojiPickerViewController(configuration: configuration)
+        let viewController = EmojiPickerViewController()
+        viewController.delegate = self
         viewController.sourceView = sender
         present(viewController, animated: true)
     }
