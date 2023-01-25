899570c2b6cf4a6a52f5c5a264c6e860c7a63883
Egor Badmaev <60363270+htmlprogrammist@users.noreply.github.com>
2023-01-25 23:22:27 +0300

    refactor: update tests
    According to the latest updates

---
+++ Tests/EmojiPickerTests/EmojiManagerTests.swift
@@ -0,0 +1,45 @@
+//
+//  EmojiManagerTests.swift
+//  EmojiPickerTests
+//
+//  Created by Егор Бадмаев on 25.01.2023.
+//
+
+import XCTest
+@testable import EmojiPicker
+
+class EmojiManagerTests: XCTestCase {
+    
+    /// SUT.
+    var emojiManager: EmojiManager!
+    
+    override func setUpWithError() throws {
+        emojiManager = EmojiManager()
+    }
+    
+    override func tearDownWithError() throws {
+        emojiManager = nil
+    }
+    
+    /**
+     Tests providing emoji set.
+     
+     Due to the fact that the tests can be run on different devices, we have to check that the answer does not come to us an empty array.
+     */
+    func testProvideEmojisMethod() throws {
+        let result = emojiManager.provideEmojis()
+        
+        XCTAssertGreaterThan(result.emojis.count, 0)
+        XCTAssertGreaterThan(result.categories.count, 0)
+        XCTAssertGreaterThan(result.aliases.count, 0)
+    }
+    
+    /**
+     Tests that device version are identical.
+     */
+    func testDeterminingDeviceVersion() throws {
+        let expectedVersion = (UIDevice.current.systemVersion as NSString).doubleValue
+        
+        XCTAssertEqual(emojiManager.deviceVersion, expectedVersion)
+    }
+}
