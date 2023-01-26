cd9d58915d11618e6a27173a7501ba8c10503600
Egor Badmaev <60363270+htmlprogrammist@users.noreply.github.com>
2023-01-26 10:40:59 +0300

    fix: ViewModel in SPM

--- Sources/EmojiPicker/ViewModel/EmojiPickerViewModel.swift
+++ Sources/EmojiPicker/ViewModel/EmojiPickerViewModel.swift
@@ -19,6 +19,8 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+import Foundation
+
 /// Protocol for a ViewModel which is being used in `EmojiPickerViewController`.
 protocol EmojiPickerViewModelProtocol {
     /// The observed variable that is responsible for the choice of emoji.
